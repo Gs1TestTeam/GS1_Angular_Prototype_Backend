f6c115aa032c0f03091c16824d06d1310d6fffe1
JongkukLee <reevekrjoon2006@gmail.com>
2018-09-27 14:08:42 -0400

    .

--- data-service_source.js
+++ data-service_source.js
@@ -12,6 +12,17 @@ var dbConfig = {
     }
 }
 
+// var dbConfig = {
+//     server: "LDM-OLGAA\\SQLEXPRESS",
+//     database: "cert_sept",
+//     user: "jlee",
+//     password: "password789!",
+//     port: "1433",
+//     options: {
+//         encrypt: false
+//     }
+// }
+
 // Load the schemas
 module.exports = function(){
      var conn;
@@ -23,7 +34,9 @@ module.exports = function(){
 
                 conn.connect().then(function() {
                     var req = new sql.Request(conn);
+
                     req.query("select count(0) as source_count from import_product i, import_product_version v where i.pid = v.pid").then( function(row) {
+                    //req.query("SELECT count(0) as source_count FROM import_product i FULL OUTER JOIN import_product_version v ON i.pid=v.pid").then( function(row) {
                         console.log("source: " + row.recordset[0].source_count);    
                         resolve(row.recordset[0].source_count);
                         conn.close();
